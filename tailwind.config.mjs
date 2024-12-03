db6247ac6c4e1a3537732aa11c5873c08729e14a
David <davidmg2512@gmail.com>
2024-12-03 18:15:13 -0500

    faq creado

--- tailwind.config.mjs
+++ tailwind.config.mjs
@@ -2,13 +2,13 @@
 import animations from '@midudev/tailwind-animations';
 
 export default {
-  content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
+  content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}',],
   theme: {
     extend: {
       animation: {
-        'scroll-left': 'scrollLeft 20s linear infinite',
       },
     },
   },
-  plugins: [animations],
+  arkMode: "class",
+  plugins: [animations,],
 };
