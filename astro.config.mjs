56c169537158ad659beadaf8b688d5a51ec1b1c9
David <davidmg2512@gmail.com>
2024-12-02 21:35:53 -0500

    header creado

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,10 +1,12 @@
 // @ts-check
 // @ts-check
 import { defineConfig } from 'astro/config';
-
 import tailwind from '@astrojs/tailwind';
+import vercel from '@astrojs/vercel/serverless';
 
 // https://astro.build/config
 export default defineConfig({
-  integrations: [tailwind()]
+  output: 'server',
+  integrations: [tailwind()],
+  adapter: vercel(),
 });
--- tailwind.config.mjs
+++ tailwind.config.mjs
@@ -1,19 +1,12 @@
 /** @type {import('tailwindcss').Config} */
-import animations from "@midudev/tailwind-animations";
+import animations from '@midudev/tailwind-animations';
 
 export default {
-  content: ["./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}"],
+  content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
   theme: {
     extend: {
-      colors: {
-        primary: "#477974",
-        background: "#E8EDEB",
-        text: "#414141",
-        "soft-primary": "#0086791c",
-      },
-      fontFamily: {
-        biryani: ["Biryani", "sans-serif"],
-        montserrat: ["Montserrat Alternates", "sans-serif"],
+      animation: {
+        test: 'bounce 1s infinite',
       },
     },
   },
